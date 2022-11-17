2334c286da1c0c24efdfdd1c41f708529da24a46
yangabrp <yg@forpeople.io>
2022-11-17 17:27:48 -0300

    fix: implementação dos conteudo do header do projeto

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,20 @@
-import * as Styled from './styles'
- import Logo from '../../../public/assets/Logo.png'
+import * as Styled from "./styles"
 export const Menu = () => {
-    return(
+  return (
     <Styled.Container>
-        <Styled.Logo src={}/>
+      <Styled.Logo>
+        <Styled.Image src="/Logo.png" />
+        <Styled.Text>Logo</Styled.Text>
+      </Styled.Logo>
+      <Styled.MenuLink>
+        <h4>Home</h4>
+        <h4>Home</h4>
+        <h4>Home</h4>
+        <h4>Home</h4>
+      </Styled.MenuLink>
+      <Styled.Social>
         <h1>ds</h1>
+      </Styled.Social>
     </Styled.Container>
-    )
+  )
 }
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,23 +1,68 @@
 import styled, {css} from "styled-components";
 import { ThemeInterface } from "../../styles/theme/interface";
 
-export const Container = styled.div`
+export const Container = styled.header`
+
  ${({ theme }: ThemeInterface) => css`
    width: 100%;
    background-color: ${theme.colors?.black};
-   height: 8rem;
+   height: 9rem;
    display: flex;
    justify-content: space-between;
    align-items: center;
    text-align: center;
+   color: white;
+   font-family: ${theme.fonts.family?.default};
     }
   `}
 `
 
-export const Logo = styled.img`
+export const Image = styled.img`
  ${({ theme }: ThemeInterface) => css`
    width: auto;
    height: 4rem;
+    }
+  `}
+`
+export const Logo = styled.div`
+ ${({ theme }: ThemeInterface) => css`
+   width: 240px;
+   height: 9rem;
+   display: flex;
+   text-align: center;
+   align-items: center;
+   justify-content: space-between;
+    }
+  `}
+`
+export const MenuLink = styled.div`
+ ${({ theme }: ThemeInterface) => css`
+   width: 340px;
+   height: 9rem;
+   display: flex;
+   text-align: center;
+   align-items: center;
+   grid-gap: 26px;
+   justify-content: center;
+    }
+  `}
+`
+export const Social = styled.div`
+ ${({ theme }: ThemeInterface) => css`
+   width: 240px;
+   height: 9rem;
+   display: flex;
+   text-align: center;
+   align-items: center;
+   grid-gap: 26px;
+   justify-content: center;
+    }
+  `}
+`
+
+export const Text = styled.h3`
+ ${({ theme }: ThemeInterface) => css`
+   font-size: ${theme.fonts.sizes?.xlarge};
     }
   `}
 `
