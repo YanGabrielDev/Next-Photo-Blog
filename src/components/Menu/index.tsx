b67a2498e6238e8db043f08e6b8664d22da54a14
Yan <yangabriel2012@gmail.com>
2022-11-17 09:11:25 -0300

    feat: adição do logo do projeto

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,9 @@
 import * as Styled from './styles'
+ import Logo from '../../../public/assets/Logo.png'
 export const Menu = () => {
     return(
     <Styled.Container>
+        <Styled.Logo src={}/>
         <h1>ds</h1>
     </Styled.Container>
     )
