03422b6ba408b98ef8283c9291150fb020521ea2
Yan <yangabriel2012@gmail.com>
2022-11-19 22:10:43 -0300

    fix: implementando chamada para a API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+import { Content } from '../src/components/Content';
+import { GridImage } from '../src/components/GridImage';
 export default function Home() {
+  const [state, setState] = useState([])
+  const getImages = () => {
+    axios.get("https://api.pexels.com/v1/search/?page=2&per_page=20&query=nature", {
+      headers: {
+        Authorization:
+          "563492ad6f917000010000017cc53e814cb84a32809bee6cbd3f58ff",
+      },
+    })
+      .then((resp) => setState(resp.data.photos))
+      
+  };
+  useEffect(()=>{
+    getImages()
+    console.log(state)
+  },[])
   return (
-    <div>
+    <Content>
+      <GridImage data={state} />
+      
+      {state?.map((img:any) => {
+        return(
+          <>
+          <h1>ada</h1>
+          </>
+        )
+      })}
     
-
-    
-    </div>
+    </Content>
   )
 }
