98cc769ceba36fc4550c6d82312b7c8c3f005ab1
Yan <yangabriel2012@gmail.com>
2022-11-19 22:11:51 -0300

    feat: implementando o estilo do component de conteudo

---
+++ src/components/Content/styles.ts
@@ -0,0 +1,12 @@
+import styled, {css} from "styled-components";
+import { ThemeInterface } from "../../styles/theme/interface";
+
+export const Container = styled.div`
+${({theme}: ThemeInterface) => css`
+ margin: 0 auto;
+ width: 100%;
+ padding-top: 20rem;
+
+
+`}
+`
