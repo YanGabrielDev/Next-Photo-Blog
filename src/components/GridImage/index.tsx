03f72f2888c16cefc2a14def48695a384ba7806d
yangabrp <yg@forpeople.io>
2022-11-21 18:01:30 -0300

    fix: alteração no gridImage para abrir modal

--- src/components/GridImage/index.tsx
+++ src/components/GridImage/index.tsx
@@ -1,13 +1,30 @@
 import * as Styled from "./styles"
+import { Image } from "./styles"
 import { GridImageInterface } from "../../interfaces/GridImage.interface"
+import { ModalContent } from "../ModalImage"
+import { useState } from "react"
 export const GridImage = ({ data, image }: GridImageInterface) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   return (
     <Styled.Container>
       <Styled.Grid>
         {data?.map((image) => (
-          <>
-              <Styled.Image src={image.src.large}  />
-          </>
+          <div key={image.id}>
+            <Image src={image.src.large} onClick={() => setIsOpen(true)} />
+
+            {isOpen && (
+              <ModalContent onClose={() => setIsOpen(false)}>
+                <Image
+                  src={image.src.large}
+                  onClick={() => {
+                    setIsOpen(true)
+                    console.log(image.id)
+                  }}
+                />
+              </ModalContent>
+            )}
+          </div>
         ))}
       </Styled.Grid>
     </Styled.Container>
