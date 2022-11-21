aaec002ac276083ac06182ee53dc133a63492148
Yan <yangabriel2012@gmail.com>
2022-11-20 21:37:46 -0300

    feat: criação da page about

---
+++ pages/About/index.tsx
@@ -0,0 +1,32 @@
+function About(){
+    return(
+        <>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        <h1>
+        flsamlas,mflsaf
+        </h1>
+        </>
+    )
+}
+
+export default About;
