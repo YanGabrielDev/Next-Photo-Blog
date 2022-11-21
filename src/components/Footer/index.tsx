754c221bd192a595e0dc88c9b5b459bfcd98f6f5
Yan <yangabriel2012@gmail.com>
2022-11-20 21:38:48 -0300

    fix: trocando heading por link para as pages

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,14 @@ export const Footer = () => {
   return (
     <Styled.Container>
       <Styled.Content>
-        <p>qjwnrjnqwkjr</p>
+        <p>
+          Desenvolvido usando{" "}
+          <Styled.Link href="https://nextjs.org" target="_blank">Nextjs</Styled.Link> e{" "}
+          <Styled.Link href="https://www.pexels.com/pt-br/" target="_blank">
+            Pexels API
+          </Styled.Link>
+          .
+        </p>
         <p>qwkrokqwr</p>
       </Styled.Content>
     </Styled.Container>
