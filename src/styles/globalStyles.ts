44cad413928dd043c78805ae601c203e173ed6db
Yan <yangabriel2012@gmail.com>
2022-11-20 21:43:36 -0300

    fix: alterando o background do body

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,10 +1,14 @@
 import { createGlobalStyle } from "styled-components";
+import { ThemeInterface } from "./theme/interface";
 
  export const GlobalStyles = createGlobalStyle`
 *{
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+}
+body{
+    background-color: ${({theme}: ThemeInterface) => theme.colors?.SoftBlack};
 }
 
 
