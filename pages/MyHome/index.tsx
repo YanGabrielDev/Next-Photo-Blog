143f721d72a9180e7e325fb74edab519cdb0a980
Yan <yangabriel2012@gmail.com>
2022-11-22 08:20:44 -0300

    fix: modificando o getImage

--- pages/MyHome/index.tsx
+++ pages/MyHome/index.tsx
@@ -3,24 +3,51 @@ import { useEffect, useState } from "react"
 import { Content } from "../../src/components/Content"
 import { GridImage } from "../../src/components/GridImage"
 function MyHome() {
-  const [state, setState] = useState([])
-  const getImages = () => {
+  const [photo, setPhoto] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const obs: any = document.querySelector('#sentinela')
+
+  // const getImages = () => {
+  //   axios
+  //     .get(`https://api.pexels.com/v1/search/?page=${currentPage}&per_page=40&query=night`, {
+  //       headers: {
+  //         Authorization:
+  //           "563492ad6f917000010000017cc53e814cb84a32809bee6cbd3f58ff",
+  //       },
+  //     })
+  //     .then((resp) => setPhoto(resp.data.photos))
+  // }
+  useEffect(() => {
     axios
-      .get("https://api.pexels.com/v1/search/?page=2&per_page=40&query=night", {
+      .get(`https://api.pexels.com/v1/search/?page=${currentPage}&per_page=40&query=night`, {
         headers: {
           Authorization:
             "563492ad6f917000010000017cc53e814cb84a32809bee6cbd3f58ff",
         },
       })
-      .then((resp) => setState(resp.data.photos))
-  }
-  useEffect(() => {
-    getImages()
-  }, [])
-  console.log(state)
+      .then((resp) => setPhoto(resp.data.photos))
+  }, [currentPage])
+
+  useEffect(() =>{
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if(entries.some((entry) => entry.isIntersecting)){
+        console.log("viu")
+        setCurrentPage((currentPageInside) => currentPageInside + 1)
+        
+      }
+    });
+    intersectionObserver.observe(obs)
+    return () => intersectionObserver.disconnect()
+  },[])
+  console.log(currentPage)
+ 
+
+
   return (
     <Content>
-      <GridImage data={state} />
+      <GridImage data={photo} />
+      <li id="sentinela" />
+      
     </Content>
   )
 }
