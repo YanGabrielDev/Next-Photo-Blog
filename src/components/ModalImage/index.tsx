e8b39caf11a401611ec793f652c11cfb52e778ad
Yan <yangabriel2012@gmail.com>
2022-11-22 08:21:39 -0300

    fix: adicionando a props image

--- src/components/ModalImage/index.tsx
+++ src/components/ModalImage/index.tsx
@@ -1,19 +1,26 @@
 import { ReactNode } from "react"
 import * as Styled from "./styles"
+import Close from "../../../Assets/Icons/Close"
+
 export interface ModalInterface {
   children?: ReactNode
   onOpen?: React.MouseEventHandler
   onClose?: React.MouseEventHandler
+  image?: any
 }
 export const Thumbnail = ({ children, onOpen }: ModalInterface) => {
   return <div onClick={onOpen}>{children}</div>
 }
 
-export const ModalContent = ({ onClose, children }: ModalInterface) => {
+export const ModalContent = ({ onClose, children, image }: ModalInterface) => {
   return (
     <Styled.Modal>
-      <Styled.close onClick={onClose}>aqui</Styled.close>
-      <Styled.modalContent>{children}</Styled.modalContent>
+      <Styled.close onClick={onClose}>
+        <Close/>
+      </Styled.close>
+      <Styled.modalContent>
+        <Styled.ImageModal src={image}/>
+      </Styled.modalContent>
     </Styled.Modal>
   )
 }
