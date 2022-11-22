de24269724a2bf3c8372df1d179e753ceaa6adde
yangabrp <yg@forpeople.io>
2022-11-22 13:42:05 -0300

    fix: alterando o modo como é feito a paginação

--- pages/MyHome/index.tsx
+++ pages/MyHome/index.tsx
@@ -4,8 +4,7 @@ import { Content } from "../../src/components/Content"
 import { GridImage } from "../../src/components/GridImage"
 function MyHome() {
   const [photo, setPhoto] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const obs: any = document.querySelector('#sentinela')
+  const [currentPage, setCurrentPage] = useState(20)
 
   // const getImages = () => {
   //   axios
@@ -19,35 +18,34 @@ function MyHome() {
   // }
   useEffect(() => {
     axios
-      .get(`https://api.pexels.com/v1/search/?page=${currentPage}&per_page=40&query=night`, {
-        headers: {
-          Authorization:
-            "563492ad6f917000010000017cc53e814cb84a32809bee6cbd3f58ff",
-        },
-      })
+      .get(
+        `https://api.pexels.com/v1/search/?page=1&per_page=${currentPage}&query=night`,
+        {
+          headers: {
+            Authorization:
+              "563492ad6f917000010000017cc53e814cb84a32809bee6cbd3f58ff",
+          },
+        }
+      )
       .then((resp) => setPhoto(resp.data.photos))
   }, [currentPage])
 
-  useEffect(() =>{
+  useEffect(() => {
     const intersectionObserver = new IntersectionObserver((entries) => {
-      if(entries.some((entry) => entry.isIntersecting)){
+      if (entries.some((entry) => entry.isIntersecting)) {
         console.log("viu")
-        setCurrentPage((currentPageInside) => currentPageInside + 1)
-        
+        setCurrentPage((currentPageInside) => currentPageInside + 20)
       }
-    });
-    intersectionObserver.observe(obs)
+    })
+    intersectionObserver.observe(document.querySelector("#sentinela")!)
     return () => intersectionObserver.disconnect()
-  },[])
+  }, [])
   console.log(currentPage)
- 
-
 
   return (
     <Content>
       <GridImage data={photo} />
-      <li id="sentinela" />
-      
+      <li id="sentinela" className="sentinela" />
     </Content>
   )
 }
