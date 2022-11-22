8ee246650d006153cffa28c3afd6c4f89c299d02
Yan <yangabriel2012@gmail.com>
2022-11-22 08:21:16 -0300

    fix: corrigindo o erro de abertura do modal

--- src/components/GridImage/index.tsx
+++ src/components/GridImage/index.tsx
@@ -5,27 +5,43 @@ import { ModalContent } from "../ModalImage"
 import { useState } from "react"
 export const GridImage = ({ data, image }: GridImageInterface) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [state, setState] = useState<any>()
+
+  const OpenImage = (image: any) => {
+    return(
+      <>
+       {/* {isOpen && (
+              <ModalContent onClose={() => setIsOpen(false)}>
+                <Image
+                  src={image}
+                  
+                  
+                  />
+              </ModalContent>
+            )} */}
+            {console.log(image)}
+      
+      
+      </>
+    )
+  }
 
   return (
     <Styled.Container>
+        {isOpen && (
+              <ModalContent onClose={() => setIsOpen(false)} image={state}/>
+                
+            )}
+
       <Styled.Grid>
         {data?.map((image) => (
+          
           <div key={image.id}>
-            <Image src={image.src.large} onClick={() => setIsOpen(true)} />
-
-            {isOpen && (
-              <ModalContent onClose={() => setIsOpen(false)}>
-                <Image
-                  src={image.src.large}
-                  onClick={() => {
-                    setIsOpen(true)
-                    console.log(image.id)
-                  }}
-                />
-              </ModalContent>
-            )}
+            <Image src={image.src.large} onClick={() => {setIsOpen(true), setState(image.src.large)}} />
+           
           </div>
         ))}
+        {/* {OpenImage(state)} */}
       </Styled.Grid>
     </Styled.Container>
   )
