445a61e871a3099100745c1272c67c8bddc885c4
yangabrp <yg@forpeople.io>
2022-11-23 15:05:41 -0300

    fix: alterando o scroll

--- pages/MyHome/index.tsx
+++ pages/MyHome/index.tsx
@@ -4,22 +4,12 @@ import { Content } from "../../src/components/Content"
 import { GridImage } from "../../src/components/GridImage"
 function MyHome() {
   const [photo, setPhoto] = useState([])
-  const [currentPage, setCurrentPage] = useState(20)
+  const [currentPage, setCurrentPage] = useState(1)
 
-  // const getImages = () => {
-  //   axios
-  //     .get(`https://api.pexels.com/v1/search/?page=${currentPage}&per_page=40&query=night`, {
-  //       headers: {
-  //         Authorization:
-  //           "563492ad6f917000010000017cc53e814cb84a32809bee6cbd3f58ff",
-  //       },
-  //     })
-  //     .then((resp) => setPhoto(resp.data.photos))
-  // }
   useEffect(() => {
     axios
       .get(
-        `https://api.pexels.com/v1/search/?page=1&per_page=${currentPage}&query=night`,
+        `https://api.pexels.com/v1/search/?page=2&per_page=${currentPage}&query=night`,
         {
           headers: {
             Authorization:
@@ -34,7 +24,7 @@ function MyHome() {
     const intersectionObserver = new IntersectionObserver((entries) => {
       if (entries.some((entry) => entry.isIntersecting)) {
         console.log("viu")
-        setCurrentPage((currentPageInside) => currentPageInside + 20)
+        setCurrentPage((currentPageInside) => currentPageInside + 1)
       }
     })
     intersectionObserver.observe(document.querySelector("#sentinela")!)
