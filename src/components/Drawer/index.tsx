52b4ce2df3838fdd084b08b5eaa6d0c65c582b38
Yan <yangabriel2012@gmail.com>
2022-11-24 22:35:08 -0300

    feat: implementando footer no drawer

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -2,8 +2,13 @@ import Close from "../../../Assets/Icons/Close"
 import { Button } from "../Button"
 import * as Styled from "./styles"
 import { DrawerInterface } from "./styles"
+import {Home, Info} from '@mui/icons-material'
+import LinkedinIcon from "../../../Assets/Icons/linkedin"
+import GithubIcon from "../../../Assets/Icons/github"
 export const Drawer = ({ isOpen, onClose }: DrawerInterface) => {
   return (
+    <>
+    
     <Styled.Container isOpen={isOpen}>
       <Styled.Header>
         <Styled.Logo>
@@ -20,6 +25,31 @@ export const Drawer = ({ isOpen, onClose }: DrawerInterface) => {
           <Close />
         </Button>
       </Styled.Header>
+      <Styled.Menu>
+        <Styled.MenuLink href={"/MyHome"}>
+          <Home/> <p>Home</p>
+        </Styled.MenuLink>
+        <Styled.MenuLink href={"/About"}>
+          <Info/> <p>About</p>
+        </Styled.MenuLink>
+    </Styled.Menu>
+      <Styled.Footer>
+      <a
+                href="https://www.linkedin.com/in/yan-gabriel-07ba581b4/"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <LinkedinIcon height="30" width="30" />
+              </a>
+              <a
+                href="https://github.com/YanGabrielDev"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <GithubIcon height="30" width="30" />
+              </a>
+      </Styled.Footer>
     </Styled.Container>
+    </>
   )
 }
