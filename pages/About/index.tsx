41884968590c298f03ab53893f4fcebc00a5d1c1
Yan <yangabriel2012@gmail.com>
2022-11-26 13:46:16 -0300

    feat: adicionando head

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 function About(){
     return(
         <>
+        <Head>
+            <title>About</title>
+        </Head>
         <h1>
         flsamlas,mflsaf
         </h1>
