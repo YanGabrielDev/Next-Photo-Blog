418a9114966396e2c59598e130f3da7a10abb634
Yan <yangabriel2012@gmail.com>
2022-11-27 16:28:36 -0300

    feat: criando component de busca

---
+++ src/components/Search/styles.ts
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+import { ThemeInterface } from "../../styles/theme/interface";
+
+export const Form = styled.div`
+${({theme}: ThemeInterface) =>css`
+display: flex;
+`}
+`
