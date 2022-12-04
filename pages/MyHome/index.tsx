3968d462cc145c518645dbb013f086e3d4e8213a
Yan <yangabriel2012@gmail.com>
2022-12-04 19:13:18 -0300

    fix: correção no funcionanmento de pesquisa do search

--- pages/MyHome/index.tsx
+++ pages/MyHome/index.tsx
@@ -4,15 +4,15 @@ import { useEffect, useState } from "react"
 import { Content } from "../../src/components/Content"
 import { GridImage } from "../../src/components/GridImage"
 import { Search } from "../../src/components/Search"
+import {PhotoInterface} from '../../src/interfaces/PhotoInterface'
 function MyHome() {
-  const [photo, setPhoto] = useState<any>([])
+  const [photo, setPhoto] = useState<Array<PhotoInterface>>([])
   const [currentPage, setCurrentPage] = useState(1)
-  const [state, setState] = useState<string>("ocean")
-
-  useEffect(() => {
-    axios
+  const [search, setSearch] = useState<string>("ocean")
+  const getPhoto = () => {
+    const res = axios
       .get(
-        `https://api.pexels.com/v1/search/?page=${currentPage}&per_page=10&query=${state}`,
+        `https://api.pexels.com/v1/search/?page=${currentPage}&per_page=10&query=${search}`,
         {
           headers: {
             Authorization:
@@ -20,8 +20,15 @@ function MyHome() {
           },
         }
       )
-      .then((resp) => setPhoto((prev: []) => [...prev, ...resp.data.photos]))
-  }, [currentPage, state])
+      .then((resp) => setPhoto((prev) => [...prev, ...resp.data.photos]))
+    return res
+  }
+
+  useEffect(() => {
+    getPhoto()
+  }, [currentPage])
+
+
   useEffect(() => {
     const intersectionObserver = new IntersectionObserver((entries) => {
       if (entries.some((entry) => entry.isIntersecting)) {
@@ -32,19 +39,31 @@ function MyHome() {
     return () => intersectionObserver.disconnect()
   }, [])
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+  }
+
+  const handleSubimit = async () => {
+    await setPhoto([])
+    await getPhoto()
+  }
+  console.log(photo)
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <Content>
-        <button>uepa</button>
-        <GridImage
-          data={photo}
-          setState={setState}
-          setPhoto={setPhoto}
-          photo={photo}
+        <Search
+          searchChange={handleChange}
+          search={search}
+          onSearch={(e: React.FormEvent<HTMLInputElement>) => {
+            e.preventDefault()
+            handleSubimit()
+          }}
         />
+        <GridImage data={photo}/>
       </Content>
     </>
   )
