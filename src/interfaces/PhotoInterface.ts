43bd6823eca1e53452fdc2f5bc80b15638cf3819
Yan <yangabriel2012@gmail.com>
2022-12-04 19:15:33 -0300

    feat: implementando interface do array de photo

---
+++ src/interfaces/PhotoInterface.ts
@@ -0,0 +1,11 @@
+export interface PhotoInterface{
+        alt: string
+        photographer_url: string
+        photographer: string
+        url: string
+        src: {
+          large: string
+        }
+    
+        index: number
+    }
