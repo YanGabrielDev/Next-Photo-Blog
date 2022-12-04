69e7d199b6e42567e7d730d6debecffd65db6f57
Yan <yangabriel2012@gmail.com>
2022-12-04 19:14:14 -0300

    fix: correção no funcionanmento de pesquisa do search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,18 +1,21 @@
 import * as Styled from './styles'
+import SearchIcon from '@mui/icons-material/Search'
 interface Search {
     searchChange: React.ChangeEventHandler<HTMLInputElement>,
     search: string,
-    onSearch: React.MouseEventHandler<HTMLButtonElement>
+    onSearch: React.MouseEventHandler<HTMLButtonElement> | any 
 }
+
 export const Search = ({search, searchChange, onSearch}: Search) => {
 return(
-    <Styled.Form>
-        <form>
-
-        <input onChange={searchChange} value={search} />
-        <button onClick={onSearch}>Submit</button>
-        </form>
-    </Styled.Form>
+    <Styled.Container>
+        <Styled.ContentForm>
+        <Styled.Form onSubmit={onSearch}>
+        <Styled.Button><SearchIcon/></Styled.Button>
+        <Styled.Input onChange={searchChange} value={search} />
+        </Styled.Form>
+        </Styled.ContentForm>
+    </Styled.Container>
 )
 
 }
